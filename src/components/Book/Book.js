514dc9f63833597a2c89888d61f364dcc6202f72
rashad Madison <rashadmadison@rashads-MacBook.local>
2018-12-28 14:34:18 -0600

    Book component up and running

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -3,7 +3,22 @@ import React, { Component } from 'react';
 class Book extends Component {
     render() {
         return (
-            <h1>Book</h1>
+          <div className="book">
+            <div className="book-top">
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.cover})` }}></div>
+              <div className="book-shelf-changer">
+                <select>
+                  <option value="move" disabled>Move to...</option>
+                  <option value="currentlyReading">Currently Reading</option>
+                  <option value="wantToRead">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">None</option>
+                </select>
+              </div>
+            </div>
+            <div className="book-title">{this.props.title}</div>
+            <div className="book-authors">{this.props.author}</div>
+          </div>
         );
     }
 }
