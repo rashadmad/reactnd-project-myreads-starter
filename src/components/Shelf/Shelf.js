d5cdd30db48eee850d9f7f57e0dbbd6ce23fb857
rashad Madison <rashadmadison@rashads-MacBook.attlocal.net>
2018-12-28 23:19:31 -0600

    Shelf and book component being used

--- src/components/Shelf/Shelf.js
+++ src/components/Shelf/Shelf.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Book from "../Book/Book.js";
 
 class Shelf extends Component {
-    render() {
-        return (
-            <h1>shelf</h1>
-        );
-    }
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.type}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+              <Book />
+            </li>
+          </ol>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Shelf;
