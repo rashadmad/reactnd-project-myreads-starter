e3dda4bed38f6d0e1b39d8b468decd679b718e37
rashad Madison <rashadmadison@rashads-macbook.attlocal.net>
2019-01-02 11:54:50 -0600

    Create new listBooks component

---
+++ src/components/Book/ListBooks.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class ListBooks extends Component {
+  render() {
+    return (
+        <ol className="books-grid">
+        </ol>
+    )
+  }
+}
+
+export default ListBooks
--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import Book from "../Book/Book.js";
+import ListBooks from "../Book/ListBooks.js";
 // API
 import * as BooksAPI from '../../BooksAPI';
 
@@ -12,66 +13,22 @@ class SearchPage extends React.Component {
    constructor(props) {
       super(props);
       this.state = {
-         books: [],
-         bookTitles: [],
-         results: [],
-         query: ""
+         books: []
       }
-   }
-   /* adds book data to books array and to the state */
-   componentDidMount() {
-      BooksAPI.getAll()
-      .then(resp => {
-         this.setState({books:resp});
-      })
-   }
-   /* Search field  */
-   updateQuery = (query) => {
-      this.setState({query: query}, this.submitSearch);
-   }
-   /* adds book data to books and the state */
-   submitSearch() {
-      if(this.state.query === '' || this.state.query === undefined) {
-         return this.setState({ results: [] })
-      }
-      BooksAPI.search(this.state.query.trim()).then(res => {
-        {/* error handleing */}
-         if(res.error) {
-            return this.setState({results: [] })
-         } else {
-            res.forEach(b => {
-               let filteredBookTitles = this.state.bookTitles.filter(B => B.title === b.title)
-               if(filteredBookTitles[0]) { b.shelf = filteredBookTitles[0].shelf }
-            });
-            return this.setState({results: res })
-         }
-      })
-   }
-   updateBook = (book, shelf) => {
-     BooksAPI.update(book, shelf)
-     .then(resp => {
-       book.shelf = shelf;
-       this.setState(state => ({
-         books: state.books.filter(b => b.id !== book.id).concat({book})
-       }));
-     });
    }
 
     render() {
+
         return (
           <div className="search-books">
             <div className="search-books-bar">
               <Link className="close-search" to="/">Close</Link>
               <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) => this.updateQuery(event.target.value)} />
+                <input type="text" placeholder="Search by title or author" value={this.state.query} />
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid">
-                {
-                  this.state.results.map((book, key) => <Book updateBook={this.updateBook} book={book} key={key} />)
-                }
-              </ol>
+                <ListBooks />
             </div>
           </div>
         );
