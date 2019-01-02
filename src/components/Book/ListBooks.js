5587584c071d0a7238741b43c187e12579e1b12f
rashad Madison <rashadmadison@rashads-macbook.attlocal.net>
2019-01-02 15:21:07 -0600

    search page refractor | display many books

--- src/components/Book/ListBooks.js
+++ src/components/Book/ListBooks.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react'
 
 class ListBooks extends Component {
   render() {
+    console.log('Right here' + 'Props', this.props)
     return (
-        <ol className="books-grid">
+        <ol className='books-grid'>
+          {this.props.books.map((book) => (
+            <li key={book.title} className='list-books-content'>
+              {book.author}
+            </li>
+          ))}
         </ol>
     )
   }
--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -8,14 +8,23 @@ import * as BooksAPI from '../../BooksAPI';
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
 
+const books = [
+  {"title": "phony", "author": "mr. phony"},
+  {"title": "bra", "author": "lady blu"},
+  {"title": "myBook", "author": "Rashad Madison"},
+  {"title": "phony", "author": "mr. phony"},
+  {"title": "phony", "author": "mr. phony"}
+]
+
 class SearchPage extends React.Component {
-  /* creates the state for books and results */
-   constructor(props) {
-      super(props);
-      this.state = {
-         books: []
-      }
-   }
+
+   // constructor(props) {
+   //    super(props);
+   //    this.state = {
+   //       books: [
+   //       ]
+   //    }
+   // }
 
     render() {
 
@@ -24,11 +33,11 @@ class SearchPage extends React.Component {
             <div className="search-books-bar">
               <Link className="close-search" to="/">Close</Link>
               <div className="search-books-input-wrapper">
-                <input type="text" placeholder="Search by title or author" value={this.state.query} />
+                <input type="text" placeholder="Search by title or author" />
               </div>
             </div>
             <div className="search-books-results">
-                <ListBooks />
+                <ListBooks books={books} />
             </div>
           </div>
         );
